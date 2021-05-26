e0f01b2ee5cccf5dd405901fa688bbec252e4c12
piyanut.rk <piyanut.rk@gmail.com>
2021-05-26 20:29:12 +0700

    Add project files.

---
+++ LabelSlide2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelSlide2
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x = this.Width;
+            label1.Text = "Please check again!";
+            label1.Font = new Font(" ", 20, FontStyle.Bold);
+            timer1.Interval = 1;
+            //timer1.Start();
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.SetBounds(x, 1, label1.Width, label1.Height);
+            x--;
+            if (x < -label1.Text.Length)
+            {
+                x = this.Width;
+            }
+        }
+    }
+}
---
+++ SlidingLabel/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace SlidingLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.slideLabel1 = new SlidingLabel.SlideLabel();
+            this.SuspendLayout();
+            // 
+            // slideLabel1
+            // 
+            this.slideLabel1.BackColor = System.Drawing.Color.Blue;
+            this.slideLabel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.slideLabel1.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.slideLabel1.ForeColor = System.Drawing.Color.White;
+            this.slideLabel1.Location = new System.Drawing.Point(0, 0);
+            this.slideLabel1.Name = "slideLabel1";
+            this.slideLabel1.Size = new System.Drawing.Size(800, 58);
+            this.slideLabel1.Slide = true;
+            this.slideLabel1.SlideTime = 10;
+            this.slideLabel1.TabIndex = 0;
+            this.slideLabel1.Text = "Hello and welcome!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.slideLabel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SlideLabel slideLabel1;
+    }
+}
+
